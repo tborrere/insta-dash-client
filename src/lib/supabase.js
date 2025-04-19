8935630d7a1189853611b5adab4da0a27b29db69
tborrere <tborrere@gmail.com>
2025-04-19 17:48:53 -0300

    adiciona conexão com Supabase

---
+++ src/lib/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://mjofjyhxwywjzyqnnjsz.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1qb2ZqeWh4d3l3anp5cW5uanN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ4MTU4NjgsImV4cCI6MjA2MDM5MTg2OH0.7-cC2YscScdGNb6VhTwtRi7cEMXTsoWGAwCLtrzesBI'
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
